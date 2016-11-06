775ffdb10b4f5f60f1d5612f6110fa53f2c27e68
kylem038 <misenckj@gmail.com>
2016-11-06 11:20:09 -0700

    Successfully fetching with promise

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
     };
+  }
+
+  componentDidMount() {
+    this.retrieveQuestions();
+  }
+
+  retrieveQuestions() {
+    const questionsUrl = () => `http://localhost:3001/quizzes`;
+    return fetch(questionsUrl())
+    .then(result => result.json())
+    .then(jsonResult => console.log(jsonResult))
+    .catch(error => console.log(error));
   }
 
   render() {
