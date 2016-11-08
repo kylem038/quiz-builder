62bc1ab69216c88b39baf7754ad2f1c8a8994920
kylem038 <misenckj@gmail.com>
2016-11-08 14:59:52 -0700

    Data is rendering

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+        quizzes: [],
+        questions: [],
     };
   }
 
@@ -17,14 +19,36 @@ class App extends Component {
     const questionsUrl = () => `http://localhost:3001/quizzes`;
     return fetch(questionsUrl())
     .then(result => result.json())
-    .then(jsonResult => console.log(jsonResult))
+    .then(result => {this.setState({
+      quizzes: result.quizzes,
+      questions: result.quizzes[0].questions,
+    });
+      console.log(result);
+    })
     .catch(error => console.log(error));
   }
 
   render() {
+    // let quiz = jsonResult.forEach((question) => { <h3>{quiz.question.title}</h3> })
     return (
       <div className="App">
-        <h1>What JavaScript Error Are You?</h1>
+        {this.state.quizzes.map(quiz => { return (
+            <div key={quiz.id}>
+              <h1>{quiz.title}</h1>
+            </div>
+          )}
+        )}
+        {this.state.questions.map(question => { return (
+          <div key={question.id}>
+            <h3>{question.title}</h3>
+            {question.answers.map((question, key) => { return (
+              <ul key={key}>
+                <li>{question.title}</li>
+              </ul>
+            )})}
+          </div>
+          )}
+        )}
       </div>
     );
   }
